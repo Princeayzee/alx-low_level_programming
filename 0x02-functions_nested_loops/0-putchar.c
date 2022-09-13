19f16ac6ca3c224d4c85285d920de19899693b5f
Princeayzee <princeabuwa@gmail.com>
2022-09-13 05:08:00 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
+
 /**
- * main - Entry point
- * Description: prints 'putchar \n'
+ * main - prints putchar
  * Return: Always 0 (success)
  */
 int main(void)
@@ -15,6 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r';
 	_putchar('\n');
-	}
+
 return (0);
 }
